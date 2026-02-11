e58fd0c10095fccfd5ff5321d18ee52a12db385f
HelHitch <olena9002@gmail.com>
2026-02-11 17:50:10 +0300

    8.5 Фикстуры Pytest

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -2,28 +2,22 @@ from http import HTTPStatus
 
 import pytest
 
-from clients.authentication.authentication_client import get_authentication_client
+from clients.authentication.authentication_client import AuthenticationClient
 from clients.authentication.authentication_schema import LoginRequestSchema, LoginResponseSchema
-from clients.users.public_users_client import get_public_user_client
-from clients.users.users_schema import CreateUserRequestSchema
+from tests.conftest import UserFixture
 from tools.assertions.authentication import assert_login_response
 from tools.assertions.base import assert_status_code
 
 
 @pytest.mark.authentication
 @pytest.mark.regression
-def test_login():
-    public_user_client = get_public_user_client()
-
-    create_user_request_schema = CreateUserRequestSchema()
-    public_user_client.create_user(create_user_request_schema)
+def test_login(function_user: UserFixture,
+               authentication_client: AuthenticationClient):
     authentication_user = LoginRequestSchema(
-        email=create_user_request_schema.email,
-        password=create_user_request_schema.password
+        email=function_user.email,
+        password=function_user.password
     )
-
-    auth_client = get_authentication_client()
-    auth_client_response = auth_client.login_api(authentication_user)
+    auth_client_response = authentication_client.login_api(authentication_user)
     login_response_data = LoginResponseSchema.model_validate_json(auth_client_response.text)
 
     assert_status_code(actual=auth_client_response.status_code,
