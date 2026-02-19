41bf9ca3fe08da353ce76e1cb647e8f270232fd8
HelHitch <olena9002@gmail.com>
2026-02-19 09:56:57 +0300

    9.4 Практикуемся в написании API автотестов. Часть 3. Практика написания API автотестов для работы с заданиями. Создание задания

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -4,7 +4,6 @@ import pytest
 
 from clients.exercises.exercises_client import ExercisesClient
 from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema
-from fixtures.exercises import ExcerciseFixture
 from tools.assertions.base import assert_status_code
 from tools.assertions.exercises import assert_create_exercise_response
 from tools.assertions.schema import validate_json_schema
@@ -15,8 +14,7 @@ from tools.assertions.schema import validate_json_schema
 class TestExercises:
 
     def test_create_exercise(self,
-                             exercises_client: ExercisesClient,
-                             function_exercise: ExcerciseFixture):
+                             exercises_client: ExercisesClient):
         request = CreateExerciseRequestSchema()
         response = exercises_client.create_exercise_api(request=request)
         response_data = CreateExerciseResponseSchema.model_validate_json(response.text)
