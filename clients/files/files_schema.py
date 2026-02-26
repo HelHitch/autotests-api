6a821e9faa9c313078f8577e329dc55ea5919451
HelHitch <olena9002@gmail.com>
2026-02-26 09:44:06 +0300

    11.1 Настройки автотестов с Pydantic.Практика работы с настройками

--- clients/files/files_schema.py
+++ clients/files/files_schema.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, HttpUrl, Field
+from pydantic import BaseModel, HttpUrl, Field, FilePath
 
 from tools import fake
 
@@ -9,7 +9,7 @@ class CreateFileRequestSchema(BaseModel):
     """
     filename: str = Field(default_factory=lambda: f"{fake.uuid4()}.png")
     directory: str = Field(default="tests")
-    upload_file: str
+    upload_file: FilePath
 
 
 class FileSchema(BaseModel):
--- fixtures/files.py
+++ fixtures/files.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 
 from clients.files.files_client import FilesClient, get_files_client
 from clients.files.files_schema import CreateFileRequestSchema, CreateFileResponseSchema
+from config import settings
 from fixtures.users import UserFixture
 
 
@@ -23,6 +24,6 @@ def files_client(function_user: UserFixture) -> FilesClient:
 
 @pytest.fixture
 def function_file(files_client: UserFixture) -> FileFixture:
-    request = CreateFileRequestSchema(upload_file="./testdata/files/test_png.jpg")
+    request = CreateFileRequestSchema(upload_file=settings.test_data.image_png_file)
     response = files_client.create_file(request=request)
     return FileFixture(request=request, response=response)
